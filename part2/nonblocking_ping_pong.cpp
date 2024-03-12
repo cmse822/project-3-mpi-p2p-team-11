70f1ef15d0ef5126f55c93983b1e5e6d584f5ef2
Nima Mirzaeian <90983089+nmirzaeian@users.noreply.github.com>
2024-03-12 11:41:08 -0400

    Update nonblocking_ping_pong.cpp

--- part2/nonblocking_ping_pong.cpp
+++ part2/nonblocking_ping_pong.cpp
@@ -12,14 +12,14 @@ double nonblocking_ping_pong(int rank, int nTrials, char* buffer, int buffSize )
             MPI_Request reqst[2];
             // communicating rank 0 to rank 1
             MPI_Isend(buffer, buffSize, MPI_CHAR, 1, 0, MPI_COMM_WORLD, &reqst[0]); 
-            MPI_Irecv(&recvBuff, buffSize, MPI_CHAR, 1, 0, MPI_COMM_WORLD, &reqst[1]);
+            MPI_Irecv(&recvBuff, buffSize, MPI_CHAR, 1, 1, MPI_COMM_WORLD, &reqst[1]);
             MPI_Waitall(2, reqst, MPI_STATUS_IGNORE); 
         }
         else {
             MPI_Request reqst[2];
             // communicating rank 1 to 0
             MPI_Irecv(&recvBuff, buffSize, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &reqst[0]);
-            MPI_Isend(buffer, buffSize, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &reqst[1]);
+            MPI_Isend(buffer, buffSize, MPI_CHAR, 0, 1, MPI_COMM_WORLD, &reqst[1]);
             MPI_Waitall(2, reqst, MPI_STATUS_IGNORE); 
         }
     }
