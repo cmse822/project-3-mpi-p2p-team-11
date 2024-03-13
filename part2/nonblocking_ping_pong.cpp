176fbefffd9a9b24e4702b44149c24aed4d2b8b4
nmirzaeian <mirzaeianima@gmail.com>
2024-03-12 22:52:44 -0400

    modified nonblocking routine

--- part2/nonblocking_ping_pong.cpp
+++ part2/nonblocking_ping_pong.cpp
@@ -1,4 +1,7 @@
 #include <mpi.h>
+#include <stdio.h> 
+#include <cstdlib>
+#include <cmath>
 
 double nonblocking_ping_pong(int rank, int nTrials, char* buffer, int buffSize ) {
     double start, end;
@@ -7,7 +10,7 @@ double nonblocking_ping_pong(int rank, int nTrials, char* buffer, int buffSize )
 
     for (int i = 0; i < nTrials; ++i) {
         // skip timing for first few iterations
-        if ( i == 15 ) start = MPI_Wtime();
+        start = MPI_Wtime();
         if ( rank == 0 ){
             MPI_Request reqst[2];
             // communicating rank 0 to rank 1
@@ -26,5 +29,26 @@ double nonblocking_ping_pong(int rank, int nTrials, char* buffer, int buffSize )
 
     end = MPI_Wtime();
 
-    return end - start;
+    return (end - start);
+}
+
+
+int main(int argc, char *argv[]) {
+           
+    MPI_Init(&argc, &argv); 
+    MPI_Comm comm = MPI_COMM_WORLD;
+      
+    int rank, buffSize, nTrials = 10000;
+    char buffer;    
+    double time;
+    MPI_Comm_rank(comm, &rank);
+    for (int j = 0; j < 8; ++j) {
+        buffSize = pow(2, j);
+        time = nonblocking_ping_pong(rank, nTrials, &buffer, buffSize);
+        printf("The total communication time is %6.9f\n",time);
+    }
+    
+    MPI_Finalize(); 
+    
+    return 0; 
 }
